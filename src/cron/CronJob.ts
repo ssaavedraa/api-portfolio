8f69fb6885fb72fec9de0d814f228f4d49102ec2
ssaavedraa <=>
2023-06-06 16:51:20 -0500

    database update at midnight every day

--- src/cron/CronJob.ts
+++ src/cron/CronJob.ts
@@ -3,14 +3,16 @@ import cron from 'node-cron'
 class CronJob {
   protected schedule: string
   protected task: () => void
+  protected options?: cron.ScheduleOptions
 
-  constructor (schedule: string, task: () => void) {
+  constructor (schedule: string, task: () => void, options?: cron.ScheduleOptions) {
     this.schedule = schedule
     this.task = task.bind(this)
+    this.options = options
   }
 
   public start () {
-    cron.schedule(this.schedule, this.task)
+    cron.schedule(this.schedule, this.task, this.options)
   }
 }
 
--- src/cron/updateRepositoryDatabase.ts
+++ src/cron/updateRepositoryDatabase.ts
@@ -1,8 +1,20 @@
+import GithubService from '../services/GithubService'
 import CronJob from './CronJob'
 
 class SyncDatabaseRepositories extends CronJob {
   constructor () {
-    super('*/1 * * * *', () => console.log('logging every minute'))
+    super('0 0 * * *', async () => {
+      try {
+        console.log('Updating database')
+        await GithubService.syncDatabase()
+        console.log('Database updated')
+      } catch (error) {
+        console.error('Error updating database', error)
+      }
+    }, {
+      scheduled: true,
+      timezone: 'America/Bogota'
+    })
   }
 }
 
