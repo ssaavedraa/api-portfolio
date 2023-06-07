55e9e062eabc77fa662c3895b8a39bcb7f3d1182
ssaavedraa <=>
2023-06-06 23:41:43 -0500

    test cors memory

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,33 @@ import { configDotenv } from 'dotenv'
 import express from 'express'
 
 import router from './routes/routesIndex'
-import SyncDatabaseRepositories from './cron/updateRepositoryDatabase'
+// import SyncDatabaseRepositories from './cron/updateRepositoryDatabase'
 
 configDotenv()
 
 const app = express()
 
 const { PORT } = process.env || 3000
-// const allowCors = process.env.FRONTEND_URL
+const allowCors = process.env.FRONTEND_URL
 
-const syncDatabaseRepositories = new SyncDatabaseRepositories()
+// const syncDatabaseRepositories = new SyncDatabaseRepositories()
 
 app.use(express.json())
 
-// app.use((_, res, next) => {
-//   res.header('Access-Control-Allow-Origin', allowCors)
-//   res.header('Access-Control-Allow-Credentials', 'true')
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   )
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-//   next()
-// })
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', allowCors)
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
 
 app.use(router)
 
-syncDatabaseRepositories.start()
+// syncDatabaseRepositories.start()
 
 app.listen(PORT, () => {
   console.log('Server is running at ', PORT)
